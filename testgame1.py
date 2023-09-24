fb511a562db513435bb3ee872cd9b38500ade8cd
guilherme-reale <116194356+guilherme-reale@users.noreply.github.com>
2023-09-24 00:35:20 -0300

    Primeira movimentação

--- testgame1.py
+++ testgame1.py
@@ -1,28 +1,61 @@
-import pygame #importa pygame
-from sys import exit #importa comando de saída
+#importa pygame
+import pygame 
+#importa comando de saída
+from sys import exit 
 
 #Inicializa pygame
 pygame.init()
 
-screen = pygame.display.set_mode((800,400)) #define o tamanho da tela
-pygame.display.set_caption('TestGame1') #Define o nome do jogo na janela
+#define o tamanho da tela
+screen = pygame.display.set_mode((800,600)) 
+
+#Define o nome do jogo na janela
+pygame.display.set_caption('As Aventuras Ecológicas de Lucas') 
+
+#controla o loop através da taxa de quadros
 clock = pygame.time.Clock()
+
+player_img = pygame.image.load("img/characters/char_down.png").convert_alpha()
+player_rec = player_img.get_rect(center = (400,300))
+
 test_font = pygame.font.Font(None,40)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
-font_surface = test_font.render('Testando Pygame',False,'Blue')
+
+#condição para o loop principal
+game_loop = True
         
-while True:
-    for event in pygame.event.get(): #verifica cada elemento em uma lista de eventos
-        if event.type == pygame.QUIT: #Verifica se o evento é de saída da janela
-            pygame.quit() #encerra o pygame
-            exit() #encerra o python
+while game_loop:
+    #verifica cada elemento em uma lista de eventos
+    for event in pygame.event.get():
+        #Verifica se o evento é de saída da janela
+        if event.type == pygame.QUIT:
+            game_loop = False
     
-    screen.blit(sky_surface,(0,0))
-    screen.blit(ground_surface,(0,300))
-    screen.blit(font_surface,(400,200))    
-    pygame.display.update() #atualiza a tela
+    pygame.draw.rect(screen,'#4b4860',pygame.Rect(0,0,800,600))
+    screen.blit(player_img,player_rec)
+    
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_w]:
+        player_rec.y-=20
+    
+    if keys[pygame.K_s]:
+        player_rec.y+=20
+    
+    if keys[pygame.K_d]:
+        player_rec.x+=20
+    
+    if keys[pygame.K_a]:
+        player_rec.x-=20
+
+    
+    #atualiza a tela    
+    pygame.display.update() 
+    #Taxa de quadros do jogo
     clock.tick(30)
     
-    
+ 
+ 
+#Sai do Pygame    
+pygame.quit()
+exit()
