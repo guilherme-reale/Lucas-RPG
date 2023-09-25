e9d118612405c3576a93a7fb523b83ee43a2b2af
guilherme-reale <116194356+guilherme-reale@users.noreply.github.com>
2023-09-25 15:32:13 -0300

    Animação de movimentação

--- testgame1.py
+++ testgame1.py
@@ -15,10 +15,33 @@ pygame.display.set_caption('As Aventuras Ecológicas de Lucas')
 #controla o loop através da taxa de quadros
 clock = pygame.time.Clock()
 
-player_img = pygame.image.load("img/characters/char_down.png").convert_alpha()
-player_rec = player_img.get_rect(center = (400,300))
+player_idle = pygame.image.load("img/characters/char_down.png").convert_alpha()
+player_rec = player_idle.get_rect(center = (400,300))
+
+player_walk1 = pygame.image.load("img/characters/Walk/Walk1.png").convert_alpha()
+player_walk2 = pygame.image.load("img/characters/Walk/Walk2.png").convert_alpha()
+player_walk3 = pygame.image.load("img/characters/Walk/Walk3.png").convert_alpha()
+player_walk4 = pygame.image.load("img/characters/Walk/Walk4.png").convert_alpha()
+player_walk5 = pygame.image.load("img/characters/Walk/Walk5.png").convert_alpha()
+player_walk6 = pygame.image.load("img/characters/Walk/walk6.png").convert_alpha()
+
+player_walk = [player_walk1,player_walk2,player_walk3,player_walk4,player_walk5,player_walk6]
+
+player_index = 0
 
 test_font = pygame.font.Font(None,40)
+
+speed = 10
+
+def player_animation():
+    global player_idle, player_index
+    if event.type == pygame.KEYDOWN:
+        if (event.key == pygame.K_LEFT) or (event.key == pygame.K_RIGHT) or (event.key == pygame.K_UP) or (event.key == pygame.K_DOWN):
+            player_index+=0.1
+            if player_index>=len(player_walk): player_index = 0
+            player_idle = player_walk[int(player_index)]
+           
+    
 
 
 #condição para o loop principal
@@ -32,21 +55,22 @@ while game_loop:
             game_loop = False
     
     pygame.draw.rect(screen,'#4b4860',pygame.Rect(0,0,800,600))
-    screen.blit(player_img,player_rec)
+    player_animation()
+    screen.blit(player_idle,player_rec)
     
     keys = pygame.key.get_pressed()
     
-    if keys[pygame.K_w]:
-        player_rec.y-=20
+    if keys[pygame.K_UP]:
+        player_rec.y-=speed
     
-    if keys[pygame.K_s]:
-        player_rec.y+=20
+    if keys[pygame.K_DOWN]:
+        player_rec.y+=speed
     
-    if keys[pygame.K_d]:
-        player_rec.x+=20
+    if keys[pygame.K_RIGHT]:
+        player_rec.x+=speed
     
-    if keys[pygame.K_a]:
-        player_rec.x-=20
+    if keys[pygame.K_LEFT]:
+        player_rec.x-=speed
 
     
     #atualiza a tela    
