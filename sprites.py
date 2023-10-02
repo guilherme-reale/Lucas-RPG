d8c1eb1f57c6d16c6a0fc89cfcb50a5eb18edf1f
guilherme-reale <116194356+guilherme-reale@users.noreply.github.com>
2023-10-02 17:59:44 -0300

    Nova classe para lidar com textos

--- sprites.py
+++ sprites.py
@@ -92,4 +92,35 @@ class DangerZone(pygame.sprite.Sprite):
         self.image.fill((0, 0, 255))
         self.rect = self.image.get_rect(center=pos)
         
+class Text:
+    def __init__(self, text, font_size, x, y):
+        self.text = text
+        self.font_size = font_size
+        self.x = x
+        self.y = y
+        self.display_surface = pygame.display.get_surface()
+
+        # Define a fonte e o tamanho da fonte
+        self.font = pygame.font.Font("font/Pixeltype.ttf", self.font_size)
+
+        # Configura a cor do texto (branco)
+        self.color = (255, 255, 255)
+
+        # Cria uma superfície de texto
+        self.rendered_text = self.font.render(self.text, True, self.color)
+
+        # Obtém o retângulo da superfície de texto
+        self.rect = self.rendered_text.get_rect()
+        self.rect.topleft = (self.x, self.y)
+
+    def update(self, new_text):
+        self.text = new_text
+        self.rendered_text = self.font.render(self.text, True, self.color)
+        self.rect = self.rendered_text.get_rect()
+        self.rect.topleft = (self.x, self.y)
+
+    def draw(self, surface):
+        surface.blit(self.rendered_text, self.rect.topleft)
+        
+        
         
